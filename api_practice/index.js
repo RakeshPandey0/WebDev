9199db6bd2aabb827387ad9a5138e1dbd498268d
RakeshPandey0 <rp901522@gmail.com>
2025-06-19 12:43:58 +0545

    CRUD REST api on users

--- api_practice/index.js
+++ api_practice/index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const fs = require("fs");
 const users = require("./MOCK_DATA.json");
-const log = require("./middleware/log.middleware")
+const log = require("./middleware/log.middleware");
 const app = express();
 const PORT = 8000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next)=> log(req, res, next));
+app.use((req, res, next) => log(req, res, next));
+
+//Routes
 app.get("/api/users", (req, res) => {
   return res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  const html = `<ul>
+        <li>${user.first_name}</li>
+    </ul>`;
+  return res.send(html);
 });
 
 app
@@ -20,15 +31,13 @@ app
     return res.json(user);
   })
   .patch((req, res) => {
-    // TODO: Update a user's data with ID = id
     const index = req.params.id - 1;
-    users[index] = {id: index+1, ...req.body};
+    users[index] = { id: index + 1, ...req.body };
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
       return res.json({ status: "success" });
     });
   })
   .delete((req, res) => {
-    // TODO: Delete user with ID = id
     const index = req.params.id - 1;
     users.splice(index, 1);
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
@@ -37,21 +46,11 @@ app
   });
 
 app.post("/api/users", (req, res) => {
-  // TODO: Create a new user
   const body = req.body;
   users.push({ id: users.length + 1, ...body });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
     return res.json({ status: "success", id: users.length });
   });
-});
-
-app.get("/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
-  const html = `<ul>
-        <li>${user.first_name}</li>
-    </ul>`;
-  return res.send(html);
 });
 
 app.get("/users", (req, res) => {
